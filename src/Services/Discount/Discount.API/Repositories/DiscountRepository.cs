b255f5624d289cffd0fd2ed3fa63a24b45a13301
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2022-01-06 16:49:55 -0300

    cretate the entity Coupon and the connection repository

---
+++ src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -0,0 +1,37 @@
+﻿using Discount.API.Entities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteDiscount(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
