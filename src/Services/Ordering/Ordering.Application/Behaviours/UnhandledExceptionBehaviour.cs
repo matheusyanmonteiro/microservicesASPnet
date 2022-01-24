2cbdb50684b599d844f717b0978eac8c886955e4
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2022-01-24 11:54:58 -0300

    Created base Behaviours

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Behaviours
+{
+    public class UnhandledExceptionBehaviour<TRequest, TResponse>
+    {
+
+    }
+
+    
+}
---
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Ordering.Application.Behaviours
+{
+    public class ValidationBehaviour<TRequest, TResponse> 
+    {
+       
+    }
+}
