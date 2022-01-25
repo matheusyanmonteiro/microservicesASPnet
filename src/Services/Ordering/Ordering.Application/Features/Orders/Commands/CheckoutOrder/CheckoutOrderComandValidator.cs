94493a76849c546affae4b960d151483b30e00b6
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2022-01-25 14:40:38 -0300

    Create Commands for Checkout Order

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderComandValidator.cs
@@ -0,0 +1,9 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderComandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderComandValidator() { }
+    }
+}
