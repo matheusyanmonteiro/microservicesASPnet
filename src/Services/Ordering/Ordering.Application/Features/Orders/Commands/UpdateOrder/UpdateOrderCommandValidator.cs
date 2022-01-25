8abcbf489c44468296a810a986b21ddf9daa7ea6
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2022-01-25 15:27:29 -0300

    create update order command base

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandValidator.cs
@@ -0,0 +1,8 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandValidator : AbstractValidator<UpdateOrderCommand>
+    {
+    }
+}
