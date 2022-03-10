84c80c7529b881551ebbc9d3c27c04cf00550534
matheus monteiro <matheusyanmonteiro@gmail.com>
2022-03-10 17:21:19 -0300

    fix bugs service running

--- src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
+++ src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
@@ -10,9 +10,7 @@ namespace Shopping.Aggregator.Extensions
         public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
         {
             if (!response.IsSuccessStatusCode)
-            {
-                throw new ApplicationException($"Something went wrong calling the API: { response.ReasonPhrase }");
-            }
+                throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
 
             var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
 
